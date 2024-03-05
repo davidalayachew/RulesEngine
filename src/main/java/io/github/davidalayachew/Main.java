82ea5891b398a0f6012a98d5fcc738c9837f2870
davidalayachew <davidalayachew@gmail.com>
2024-03-05 04:39:51 -0500

    Next

--- src/main/java/io/github/davidalayachew/Main.java
+++ src/main/java/io/github/davidalayachew/Main.java
@@ -3,22 +3,6 @@ package io.github.davidalayachew;
 public class Main
 {
 
-   Main()
-   {
-   
-      do
-      {
-      
-         final int blah = 3;
-      
-      }
-      
-      while (blah > 2);
-   
-   }
-
-   int blah = 2;
-
    public static void main(String[] args)
    {
    
